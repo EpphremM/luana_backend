38cdbf4a6bcc674a74bbd8e606099596ec8865a1
EpphremM <mandefroephrem48@gmail.com>
2025-04-24 08:53:22 -0900

    fixing on admin data update after winner checked if the cartela is winner or not

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 import intializeConnection from "./database/data.source";
-import express, {Application} from "express";
-import bodyParser from "body-parser";
 import dotenv from "dotenv"
 import "reflect-metadata";
 import app from "./app"
-import cors from 'cors'
 dotenv.config();
 
 const port=process.env.PORT||3000;
