b235271e7a621672a4c62349f79284c5d13b5648
EpphremM <mandefroephrem48@gmail.com>
2025-04-25 21:16:02 -0900

    fixing on authentication middleware

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ class App {
         // CORS should be one of the first middlewares
         this.app.use(cookieParser());
         this.app.use(cors({
-            origin: ["http://localhost:3001","http://localhost:3002","https://luana-bingo.vercel.app/"],
+            origin: ["http://localhost:3001","http://localhost:3002","https://luana-bingo.vercel.app/","https://luana-bingo.vercel.app"],
             credentials: true,
             methods: ["POST", "GET", "PATCH", "DELETE", "OPTIONS"] // Added OPTIONS
         }));
