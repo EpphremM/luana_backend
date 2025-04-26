fc60c21cd5435bb20e086876f7db69dae411d4ce
EpphremM <mandefroephrem48@gmail.com>
2025-04-26 04:45:51 -0900

    adding index roting

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -21,19 +21,19 @@ declare global {
 }
 
 const setAuthCookies = (res: Response, accessToken: string, refreshToken: string): void => {
-  const isProduction = process.env.NODE_ENV === 'production';
+ 
   
   res.cookie('access_token', accessToken, {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 15 * 60 * 1000, // 15 minutes
   });
 
   res.cookie('refresh_token', refreshToken, {
     httpOnly: true,
-    secure: false,
-    sameSite: isProduction ? "none" : "lax",
+    secure: true,
+    sameSite:"none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
