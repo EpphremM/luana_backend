a789885341b78c202d6c741f66f3eedd3fbaff09
EpphremM <mandefroephrem48@gmail.com>
2025-06-28 02:27:25 -0900

    changeing api url

--- src/database/data.source.ts
+++ src/database/data.source.ts
@@ -5,12 +5,12 @@ import { createDefaultCompany } from "../controller/company.controller";
 
 export const AppDataSource: DataSource = new DataSource({
   type: "postgres",
-  // url:ENV.database_url,
- host: ENV.db_host,
-  username: ENV.db_username,
-  port: parseInt(ENV.db_port || "5432"),
-  database: ENV.db_name,
-  password: ENV.db_password,
+  url:ENV.database_url,
+//  host: ENV.db_host,
+//   username: ENV.db_username,
+//   port: parseInt(ENV.db_port || "5432"),
+//   database: ENV.db_name,
+//   password: ENV.db_password,
   entities: [__dirname + '/entities/*.entity.{ts,js}',__dirname+"/../payment/entities/**/*.ts"],
   migrations: [__dirname + "/migration/**/*.ts"],
   synchronize: true,
